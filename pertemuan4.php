1316097ea7b4f87e5c1d66347c530d5b2b3e6879
Biyuu0-0 <nur.zahra1012@gmail.com>
2026-04-16 15:06:15 +0700

    Add files via upload

---
+++ pertemuan4.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 4</title>
+</head>
+<body>
+    <h3>Praktikum Pertemuan 4</h3>
+    <table border="1" cellpadding="8">
+        <tr>
+            <th colspan="4">Daftar Smartphone</th>
+        </tr>
+        <tr>
+            <th>No</th>
+            <th>Nama Brand</th>
+            <th>Gambar</th>
+            <th>Keterangan</th>
+        </tr>
+        <tr>
+            <td rowspan="4">1</td>
+            <td rowspan="4">Apple</td>
+            <td rowspan="4"><img src="iPhone.jpg" alt="Foto Hp Apple" width="100"></td>
+            <tr>
+                <td>iPhone 16 Pro Max</td></tr>
+            <tr>
+                <td>Dengan desain titanium yang memukau. Kontrol kamera. Dolby Vision 4k 120 fps. Dan chip A18 Pro</td></tr>
+            <tr>
+                <td>Rp 20.000.000</td></tr>
+        </tr>
+        <tr>
+            <td rowspan="4">2</td>
+            <td rowspan="4">Samsung</td>
+            <td rowspan="4"><img src="samsung.jpg" alt="Foto Hp Samsung" width="100"></td>
+            <tr>
+                <td>Samsung Galaxy S24 Ultra</td></tr>
+            <tr>
+                <td>Hp premium dengan layar besar, S pen, kamera canggih, dan performa sangat mewah</td></tr>
+            <tr>  
+                <td>Rp 18.000.000</td></tr>
+        </tr>
+        <tr>
+            <td rowspan="4">3</td>
+            <td rowspan="4">Xiaomi</td>
+            <td rowspan="4"><img src="xiaomi.jpg" alt="Foto Hp Xiaomei" width="100"></td>
+            <tr>
+                <td>Xiaomi 12 8/256</td></tr>
+            <tr>
+                <td>Hp dengan performa kencang, desain elegan, dan harga lebih terjangkau.</td></tr>
+            <tr>
+                <td>Rp 8.000.000</td></tr>
+        </tr>
+    </table>
+</body>
+</html>
